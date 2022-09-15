057c11e0a42cc2f9570f878676690ae8d6e25b45
Roos Cocolien <r.steigenga@gmail.com>
2022-09-15 11:40:00 +0200

    refactor(header): desktop version

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { Menu, Search, User } from 'react-feather';
 
-import { QUERIES } from '../../constants';
+import { QUERIES, COLORS } from '../../constants';
 
 import MaxWidthWrapper from '../MaxWidthWrapper';
 import Logo from '../Logo';
@@ -29,7 +29,19 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <ActionGroupDesktop>
+          <button>
+            <Search size={24}/>
+          </button>
+          <button>
+            <Menu size={24}/>
+          </button>
+        </ActionGroupDesktop>
         <Logo />
+        <SubscribeGroupDesktop>
+          <SubscribeButton>Subscribe</SubscribeButton>
+          <LoginLink href="/login">Already a subscriber?</LoginLink>
+        </SubscribeGroupDesktop>
       </MainHeader>
     </header>
   );
@@ -39,6 +51,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -56,6 +72,25 @@ const ActionGroup = styled.div`
   */
   svg {
     display: block;
+    color: var(--color-gray-100);
+  }
+`;
+
+const ActionGroupDesktop = styled.div`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+    gap: 24px;
+    
+    /*
+    FIX: Remove the inline spacing that comes with
+    react-feather icons.
+    */
+    svg {
+      display: block;
+      color: var(--color-gray-900);
+    }
   }
 `;
 
@@ -65,6 +100,39 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  @media ${QUERIES.desktopAndUp} {
+    justify-content: space-between;
+    align-items: center;
+  }
 `;
+
+const SubscribeGroupDesktop = styled.div`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 8px;
+  }
+`;
+
+const SubscribeButton = styled(Button)`
+  text-transform: uppercase;
+
+`
+
+const LoginLink = styled.a`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: block;
+    text-decoration: underline;
+    font-style: italic;
+    color: var(--color-gray-700);
+  }
+`
 
 export default Header;
